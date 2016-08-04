1efe75c374473f25400faf82a526d8ba18eec576
wangqingqiang <qingqiang.wang@qq.com>
2016-08-05 00:54:24 +0800

    添加主页
    添加主页

--- app.js
+++ app.js
@@ -2,34 +2,21 @@
  * Created by wqq on 2016/7/14.
  */
 var express=require('express');
-var albumRouter=require('./Controllers/albumRouter.js');
-var crawlerRouter=require('./Controllers/crawlerRouter.js');
+var ejs=require('ejs');
+var router=require('./routes/router.js');
 var app=express();
 
-app.set('views','./Views');
-app.set('view engine','ejs');
+app.set('views','./views');
+app.engine('.html', ejs.__express);
+app.set('view engine','html');
 //设置静态服务路由
 //注意此处是 /static路由 ，但是目录是 ./Public，一个带 '.'，一个不带
-app.use('/static',express.static('./Public'));
+app.use('/static',express.static('./public'));
 
+router(app);
 // 路由前面都是没有'.'的，否则匹配不上，
 //注意此处不是app.use，app.use不是精确匹配，它可以匹配到书写的路由后面的子路由，
 // 例如app.use(/admin,callback)可以匹配到 /admin/adminlist/adminid等
-app.get('/',albumRouter.showAlbumList);
-app.get('/favicon.ico',function(req,res){res.end();});
 
-app.get('/crawler',crawlerRouter.showCrawlerPage)
-app.post('/getJobs',crawlerRouter.getJobList)
-
-//设置相册详情页路由
-app.get('/albums',albumRouter.showAlbumList); //相册列表页
-app.get('/uploadAlbum',albumRouter.showUploadPage); //上传图片页
-app.post('/uploadAlbum',albumRouter.uploadImg); // 上传图片
-app.get('/:albumName',albumRouter.showAlbumPhoto) //某一个相册页
-app.get('/upload/album/:albumName/:id',albumRouter.getPhoto); //获取相册内的某个图片
-
-//异常页面
-app.get('/500error',albumRouter.show500)
-app.use(albumRouter.show404);
 
 app.listen(process.env.PORT || 5050)
---
+++ routes/commonRoute.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/8/5 0005.
+ */
+function showIndexPage(req, res, next) {
+    res.render('index');
+}
+
+module.exports = {
+    showIndexPage
+};
