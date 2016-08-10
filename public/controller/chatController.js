288801b573c224823f97ad19247cb514fcc4e56e
wangqingqiang <qingqiang.wang@qq.com>
2016-08-10 23:41:53 +0800

    在线聊天室
    样式有问题

--- public/controller/chatController.js
+++ public/controller/chatController.js
@@ -3,6 +3,11 @@
  */
 ;angular.module('app.chatRoom.controller',[])
 .controller('chatController',['$scope','$state',function($scope,$state){
+    $scope.nickName='';
+    $scope.messageContent='';
+    $scope.messages=[];
+    $scope.uid=new Date().getTime();
+
     $scope.setNickName= function () {
         var nickName=prompt('请输入一个昵称：');
         if(nickName===null){
@@ -14,14 +19,24 @@
         }
     };
     $scope.setNickName();
-    //$scope.messages=[];
-    //var socket=io();
-    //socket.on('message',function(msg){
-    //   // $scope.$apply(function(){
-    //        $scope.messages.push(msg);
-    //    //});
-    //});
-    //$scope.sendMsg=function(){
-    //    socket.emit('message',$('#message').val());
-    //}
+
+    var socket=io();
+    socket.on('message',function(msg){
+        $scope.$apply(function(){
+            $scope.messages.push(msg);
+        });
+    });
+    $scope.sendMsg=function(){
+        if(!$scope.messageContent){
+            return false;
+        }
+        socket.emit('message',{nickName:$scope.nickName,msg: $scope.messageContent,uid:$scope.uid});
+        $scope.messageContent='';
+    };
+    $scope.keyDown=function(e){
+        var event=e||window.event;
+        if(event.keyCode==13){
+            $scope.sendMsg();
+        }
+    }
 }])
